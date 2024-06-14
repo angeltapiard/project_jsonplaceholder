7a12b13237d9f8c15246bdf8d378bb77267a7d95
ANGEL MIGUEL TAPIA <O99ZZ92@uasd.local>
2024-06-14 10:00:37 -0600

    concluido

--- vistas/todos.js
+++ vistas/todos.js
@@ -6,13 +6,24 @@ fetch(`https://jsonplaceholder.typicode.com/todos?userId=${userId}`)
     .then(response => response.json())
     .then(todos => {
         todos.forEach(todo => {
-            const todoDiv = document.createElement('div');
-            todoDiv.classList.add('todo', 'card','mb-3', 'p-3', todo.completed ? 'true' : 'false');
+            if(todo.completed){
+                todosContainer.innerHTML += `
+                <div class="alert alert-success" role="alert">
+                ${todo.title}
+                </div>`;
+            }else{
+                todosContainer.innerHTML += `
+                <div class="alert alert-danger" role="alert">
+                ${todo.title}
+                </div>`;
+            }
+            //const todoDiv = document.createElement('div');
+            //todoDiv.classList.add('todo', 'card','mb-3', 'p-3', todo.completed ? 'true' : 'false');
 
-            todoDiv.innerHTML = `
-                <h5 class="color"><hr>Title: ${todo.title} <hr>Completed: ${todo.completed}<hr></h6>
-            `;
+            //todoDiv.innerHTML = `
+               // <h5 class="color"><hr>Title: ${todo.title} <hr>Completed: ${todo.completed}<hr></h6>
+            //`;
 
-            todosContainer.appendChild(todoDiv);
+            //</hr></hr>todosContainer.appendChild(todoDiv);
         });
     });
